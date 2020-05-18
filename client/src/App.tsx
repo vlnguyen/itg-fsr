989b17c222208629ecd5c5af985075b8f2d6fb6d
Vincent Nguyen <VincentLocNguyen@gmail.com>
2020-05-17 19:38:51 -0500

    Create inputs and input actions.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,67 +10,133 @@ enum Arrows {
   RIGHT = 3
 }
 
-function SensitivityForm() {
-  const [thresholds, setThreshholds] = useState([]);
- 
-  useEffect(() => {
-    fetch("http://192.168.1.128:5555/thresholds")
-      .then(resp => resp.json())
-      .then(data => setThreshholds(data.values))
-  }, []);
-
-  if (thresholds.length === 0) {
-    return <p>Loading thresholds...</p>
-  }
-
+function PerArrowSensitivityInput (arrow: Arrows, thresholds: number[], setThresholds: React.Dispatch<React.SetStateAction<number[]>>) {
+  const { inputName, divClassName } = getPerArrowProperties(arrow);
   return (
-    <form className="grid" onSubmit={(e) => { }}>
-      <div className="grid__item" />
-        <div className="grid__verticalArrow">
-          <input 
-            type="number"
-            name="thresholdUp" 
-            value={thresholds[Arrows.UP]}
-          />
-        </div>
-        <div className="grid__item" />
-        <div className="grid__leftArrow">
-          <input 
-            type="number"
-            name="thresholdLeft" 
-            value={thresholds[Arrows.LEFT]}
-          />
-        </div>
-        <div className="grid__item">
-          <img src={padImage} className="App-image" alt="pad" />
-        </div>
-        <div className="grid__rightArrow">
-          <input 
-            type="number"
-            name="thresholdRight" 
-            value={thresholds[Arrows.RIGHT]}
-          />
-        </div>
-        <div className="grid__item" />
-        <div className="grid__verticalArrow">
-          <input 
-            type="number"
-            name="thresholdDown" 
-            value={thresholds[Arrows.DOWN]}
-          />
-        </div>
-      <div className="grid__item" />
-    </form>
-  );
-}
-
-function App() {
-  return (
-    <div className="App">
-      <SensitivityForm />
-        hey man
+    <div className={divClassName}>
+      <input 
+        type="number"
+        name={inputName}
+        value={thresholds[arrow]}
+        onChange={e => {
+          const newThresholds = [...thresholds];
+          newThresholds[arrow] = parseInt(e.target.value);
+          setThresholds(newThresholds);
+        }}
+      />
+      {PlusMinusButtons(arrow, thresholds, setThresholds)}
     </div>
   );
 }
 
+const PlusMinusButtons = (
+  arrow: Arrows, 
+  thresholds: number[],
+  setThresholds: React.Dispatch<React.SetStateAction<number[]>>) => {
+    return (
+      <span>
+        <button onClick={e => handleLowerThreshold(e, arrow, thresholds, setThresholds)}>-</button>
+        <button onClick={e => handleRaiseThreshold(e, arrow, thresholds, setThresholds)}>+</button>
+      </span>
+    );
+};
+
+const handleLowerThreshold = (
+  e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  arrow: Arrows,
+  thresholds: number[],
+  setThresholds:React.Dispatch<React.SetStateAction<number[]>>) => {
+    handleChangeThreshold(e, arrow, thresholds[arrow] - 10, thresholds, setThresholds);
+};
+
+const handleRaiseThreshold = (
+  e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  arrow: Arrows,
+  thresholds: number[],
+  setThresholds:React.Dispatch<React.SetStateAction<number[]>>) => {
+    handleChangeThreshold(e, arrow, thresholds[arrow] + 10, thresholds, setThresholds);
+};
+
+const handleChangeThreshold = (
+  e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+  arrow: Arrows,
+  newValue: number,
+  thresholds: number[],
+  setThresholds:React.Dispatch<React.SetStateAction<number[]>>) => {
+    e.preventDefault();
+    const newThresholds = [...thresholds];
+    newThresholds[arrow] = newValue;
+    setThresholds(newThresholds);
+}
+
+const getPerArrowProperties = (arrow: Arrows) => {
+  let inputName:string, divClassName: string;
+  switch(arrow) {
+    case Arrows.LEFT:
+      inputName = "thresholdLeft";
+      divClassName = "grid__leftArrow";  
+      break;
+    case Arrows.DOWN:
+      inputName = "thresholdDown";
+      divClassName = "grid__verticalArrow";  
+      break;
+    case Arrows.UP:
+      inputName = "thresholdUp";
+      divClassName = "grid__verticalArrow";  
+      break;
+    case Arrows.RIGHT:
+      inputName = "thresholdRight";
+      divClassName = "grid__Arrow";  
+      break;
+    case Arrows.NONE:
+      inputName = "";
+      divClassName = "";  
+      break;
+  }
+
+  return { inputName: inputName, divClassName: divClassName };
+};
+
+const handleSubmit = (thresholds:number[]) => {
+  console.log(thresholds);
+}
+
+function App() {
+  const [thresholds, setThresholds] = useState<number[]>([]);
+ 
+  useEffect(() => {
+    fetch("http://192.168.1.128:5555/thresholds")
+      .then(resp => resp.json())
+      .then(data => setThresholds(data.values))
+  }, []);
+
+  if (thresholds.length === 0) {
+    return (
+      <div className="App">
+        <p>Loading thresholds...</p>
+      </div>
+    );
+  }
+
+  return (    
+    <div className="App">
+      <div className="grid">
+        <div className="grid__item" />
+        {PerArrowSensitivityInput(Arrows.UP, thresholds, setThresholds)}
+        <div className="grid__item" />
+        {PerArrowSensitivityInput(Arrows.LEFT, thresholds, setThresholds)}
+        <div className="grid__item">
+          <img src={padImage} className="App-image" alt="pad"/>
+        </div>
+        {PerArrowSensitivityInput(Arrows.RIGHT, thresholds, setThresholds)}
+        <div className="grid__item" />
+        {PerArrowSensitivityInput(Arrows.DOWN, thresholds, setThresholds)}
+        <div className="grid__item" />
+      </div>
+      <button className="submitButton" onClick={() => handleSubmit(thresholds)}>
+        Set Thresholds
+      </button>
+    </div>
+  );
+}
 export default App;
